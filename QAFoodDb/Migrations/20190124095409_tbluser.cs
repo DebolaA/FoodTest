647a7e94948d314767344d5e27edf182fffb5789
Debola <35138680+DebolaA@users.noreply.github.com>
2019-01-24 10:04:56 +0000

    Add files via upload

---
+++ QAFoodDb/Migrations/20190124095409_tbluser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QAFoodDb.Migrations
+{
+    public partial class tbluser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "UserId", "CreatedBy", "CreatedOn", "Email", "FName", "LName", "RoleId" },
+                values: new object[] { 1, null, null, "user@yahoo.com", null, null, null });
+
+            migrationBuilder.InsertData(
+             table: "User",
+             columns: new[] { "Email" },
+             values: new object[] { "dee@yahoo.com" });
+                }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
